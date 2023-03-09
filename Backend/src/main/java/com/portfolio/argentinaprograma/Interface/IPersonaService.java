fcf4e6e9e2c4124b6ca5a07ec7e6be5c3fb2fd0b
Nicolas Koch <119958241+nicolaskochmain@users.noreply.github.com>
2023-03-09 18:59:52 -0300

    finished backend

---
+++ Backend/src/main/java/com/portfolio/argentinaprograma/Interface/IPersonaService.java
@@ -0,0 +1,14 @@
+package com.portfolio.argentinaprograma.Interface;
+
+import com.portfolio.argentinaprograma.Entity.Persona;
+import java.util.List;
+
+public interface IPersonaService {
+    public List<Persona> getPersona();
+    
+    public void savePersona(Persona persona);
+    
+    public void deletePersona(Long id);
+    
+    public Persona findPersona(Long id);
+}
